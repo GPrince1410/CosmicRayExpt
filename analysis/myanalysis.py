42b5b8029af4883e95af79f6b341d7978464d325
GPrince1410 <72617107+GPrince1410@users.noreply.github.com>
2020-10-09 17:27:07 +0100

    Update myanalysis.py

--- analysis/myanalysis.py
+++ analysis/myanalysis.py
@@ -55,50 +55,6 @@ Count(count700,'0Chan700.dat')
 Count(count600,'0Chan600.dat')
 Count(count500,'0Chan500.dat')
 
-print("Counts by channel")
-print("Channel 0 : {} ".format(count[0]))
-print("Channel 1 : {} ".format(count[1]))
-print("Channel 2 : {} ".format(count[2]))
-print("Channel 3 : {} ".format(count[3]))
-
-# now find concidences betwen two channels (0 and 1)
-n_coinc = 0
-for event in events:
-    found0 = False
-    found1 = False
-    for pulse in event.pulses:
-        # only count rising edges
-        if pulse.edge==0 and pulse.chan == 0:
-            found0 = True
-        if pulse.edge==0 and pulse.chan == 1:
-            found1 = True
-    if found0 and found1:
-        n_coinc += 1
-            
-print("N (0,1) coincidences : {}".format(n_coinc))
-
-# get some pulse time information
-dts = []
-for event in events:
-    found0 = False
-    found1 = False
-    time0 = 0.
-    time1 = 0.
-    for pulse in event.pulses:
-        # only count rising edges
-        if pulse.edge==0 and pulse.chan == 0:
-            found0 = True
-            time0 = pulse.time
-        if pulse.edge==0 and pulse.chan == 1:
-            found1 = True
-            time1 = pulse.time
-    if found0 and found1:
-        dts.append(abs(time1-time0))
-
-# print some summary info
-print("Mean delta-t : {}".format(np.mean(dts)))
-print("Std dev delta-t : {}".format(np.std(dts)))
-
 bins = [count1000[0], count900[0],count800[0],count700[0],count600[0],count500[0]]
 thresh = [1000,900,800,700,600,500]
 plt.plot(thresh, bins)
