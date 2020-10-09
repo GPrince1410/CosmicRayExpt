60971695df3c9ff21c0f6adb3034bbc9c88382db
GPrince1410 <72617107+GPrince1410@users.noreply.github.com>
2020-10-09 16:21:21 +0100

    Create myanalysis.py

---
+++ myanalysis.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  9 15:52:33 2020
+
+@author: glads
+"""
+
+#!/usr/bin/env python
+
+# data analysis example program
+# Including some examples of how to use DataFrames from pandas
+#
+# Usage :
+# python analysis.py -i test.dat
+
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+
+from event import Event, Pulse
+
+parser = argparse.ArgumentParser(description='Analyse CSV file')
+parser.add_argument("-i", "--in_file", help="input file")
+parser.add_argument("-o", "--out_file", help='output file')
+parser.add_argument("-n", "--n_max", help='max number of lines to process')
+
+args = parser.parse_args()
+
+print("Starting analysis")
+            
+# open the file
+ifile = open(args.in_file, 'rb')
+events= pickle.load(ifile)
+n_events= len(events)
+
+10file = open("0Chan1000.dat")
+10events= pickle.load(10file)
+n10_events= len(10events)
+9file = open("0Chan900.dat")
+9events= pickle.load(9file)
+n9_events= len(9events)
+8file = open("0Chan800.dat")
+8events= pickle.load(8file)
+n8_events= len(8events)
+7file = open("0Chan700.dat")
+7events= pickle.load(7file)
+n7_events= len(7events)
+6file = open("0Chan600.dat")
+6events= pickle.load(6file)
+n6_events= len(6events)
+5file = open("0Chan500.dat")
+5events= pickle.load(5file)
+n5_events= len(5events)
+
+
+print("Read {} events from file".format(n_events))
+
+# example event loop
+count = [0,0,0,0]
+count1000 = [0, 0, 0, 0] 
+count900 = [0, 0, 0, 0]
+count800 = [0, 0, 0, 0]
+count700 = [0, 0, 0, 0]
+count600 = [0, 0, 0, 0]
+count500 = [0, 0, 0, 0]# counts per channel
+
+for event in events:
+    for pulse in event.pulses:
+        # only count rising edges
+        if pulse.edge == 0:
+            count[pulse.chan] += 1
+for event in 10events:
+    for pulse in event.pulses:
+        # only count rising edges
+        if pulse.edge == 0:
+            count1000[pulse.chan] += 1
+for event in 9events:
+    for pulse in event.pulses:
+        # only count rising edges
+        if pulse.edge == 0:
+            count900[pulse.chan] += 1
+for event in 8events:
+    for pulse in event.pulses:
+        # only count rising edges
+        if pulse.edge == 0:
+            count800[pulse.chan] += 1
+for event in 7events:
+    for pulse in event.pulses:
+        # only count rising edges
+        if pulse.edge == 0:
+            count700[pulse.chan] += 1
+for event in 6events:
+    for pulse in event.pulses:
+        # only count rising edges
+        if pulse.edge == 0:
+            count600[pulse.chan] += 1
+for event in 5events:
+    for pulse in event.pulses:
+        # only count rising edges
+        if pulse.edge == 0:
+            count500[pulse.chan] += 1
+print("Counts by channel")
+print("Channel 0 : {} ".format(count[0]))
+print("Channel 1 : {} ".format(count[1]))
+print("Channel 2 : {} ".format(count[2]))
+print("Channel 3 : {} ".format(count[3]))
+
+# now find concidences betwen two channels (0 and 1)
+n_coinc = 0
+for event in events:
+    found0 = False
+    found1 = False
+    for pulse in event.pulses:
+        # only count rising edges
+        if pulse.edge==0 and pulse.chan == 0:
+            found0 = True
+        if pulse.edge==0 and pulse.chan == 1:
+            found1 = True
+    if found0 and found1:
+        n_coinc += 1
+            
+print("N (0,1) coincidences : {}".format(n_coinc))
+
+# get some pulse time information
+dts = []
+for event in events:
+    found0 = False
+    found1 = False
+    time0 = 0.
+    time1 = 0.
+    for pulse in event.pulses:
+        # only count rising edges
+        if pulse.edge==0 and pulse.chan == 0:
+            found0 = True
+            time0 = pulse.time
+        if pulse.edge==0 and pulse.chan == 1:
+            found1 = True
+            time1 = pulse.time
+    if found0 and found1:
+        dts.append(abs(time1-time0))
+
+# print some summary info
+print("Mean delta-t : {}".format(np.mean(dts)))
+print("Std dev delta-t : {}".format(np.std(dts)))
+
+bins = [count1000[0], count900[0],count800[0],count700[0],count600[0],count500[0]]
+thresh = [1000,900,800,700,600,500]
+plt.plot(bins, thresh)
+plt.ylabel("Events")
+plt.xlabel('Threshold')
+plt.show()
