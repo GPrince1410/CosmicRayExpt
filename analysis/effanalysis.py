cb17e29f2c27295855f9160a6c6148af0b85463c
GPrince1410 <72617107+GPrince1410@users.noreply.github.com>
2020-10-23 12:32:53 +0100

    Update effanalysis.py

--- analysis/effanalysis.py
+++ analysis/effanalysis.py
@@ -14,66 +14,72 @@ Created on Fri Oct  9 15:52:33 2020
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
 from event import Event, Pulse
 
+parser = argparse.ArgumentParser(description='Analyse CSV file')
+parser.add_argument("-i", "--in_file", help="input file")
+parser.add_argument("-o", "--out_file", help='output file')
+parser.add_argument("-n", "--n_max", help='max number of lines to process')
+
+args = parser.parse_args()
+
 print("Starting analysis")
 
+# open the file
+ifile = open(args.in_file, 'rb')
+events= pickle.load(ifile)
+n_events= len(events)
+
+print("Read {} events from file".format(n_events))
+
 # example event loop
-count1000 = [0, 0, 0, 0] 
-count900 = [0, 0, 0, 0]
-count800 = [0, 0, 0, 0]
-count700 = [0, 0, 0, 0]
-count600 = [0, 0, 0, 0]
-count500 = [0, 0, 0, 0]# counts per channel
-count400 = [0, 0, 0, 0]
-count300 = [0, 0, 0, 0]
-count200 = [0, 0, 0, 0]
-count100 = [0, 0, 0, 0]
+count = [0, 0, 0, 0]  # counts per channel
 
-def Count(countl,file):
+for event in events:
+    for pulse in event.pulses:
+        # only count rising edges
+        if pulse.edge == 0:
+            count[pulse.chan] += 1
 
-    ifile = open(file)
-    events= pickle.load(ifile)
-    
-    for event in events:
-        for pulse in event.pulses:
-            # only count rising edges
-            if pulse.edge == 0:
-                countl[pulse.chan] += 1
+print("Counts by channel")
+print("Channel 0 : {} ".format(count[0]))
+print("Channel 1 : {} ".format(count[1]))
+print("Channel 2 : {} ".format(count[2]))
+print("Channel 3 : {} ".format(count[3]))
 
-    return countl
-    
-Count(count1000, '1000.dat')
-Count(count900,'900.dat')
-Count(count800,'800.dat')
-Count(count700,'700.dat')
-Count(count600,'600.dat')
-Count(count500,'500.dat')
-Count(count400,'400.dat')
-Count(count300,'300.dat')
-Count(count200,'200.dat')
-Count(count100, '100.dat')
+# now find concidences betwen two channels (0 and 1)
+n0_coinc = 0
+n1_coinc = 0
+n2_coinc = 0
+n3_coinc = 0
+for event in events:
+    found0 = False
+    found1 = False
+    for pulse in event.pulses:
+        # only count rising edges
+        if pulse.edge==0 and pulse.chan == 0:
+            found0 = True
+        if pulse.edge==0 and pulse.chan == 1:
+            found1 = True
+        if pulse.edge==0 and pulse.chan == 2:
+            found2 = True
+        if pulse.edge==0 and pulse.chan == 3:
+            found3 = True
+    if found1 and found2:
+        n0_coinc += 1
+    if found0 and found2:
+        n1_coinc += 1
+    if found1 and found3:
+        n2_coinc += 1
+    if found0 and found1:
+        n3_coinc += 1
+        
+print("N (0,1) coincidences : {}".format(n3_coinc))
 
-counts0 = [count1000[0], count900[0],count800[0],count700[0],count600[0],count500[0],count400[0],count300[0],count200[0],count100[0]]
-counts1 = [count1000[1], count900[1],count800[1],count700[1],count600[1],count500[1],count400[1],count300[1],count200[1],count100[1]]
-counts2 = [count1000[2], count900[2],count800[2],count700[2],count600[2],count500[2],count400[2],count300[2],count200[2],count100[2]]
-counts3 = [count1000[3], count900[3],count800[3],count700[3],count600[3],count500[3],count400[3],count300[3],count200[3],count100[3]]
-thresh = [1000,900,800,700,600,500,400,300,200,100]
-time = 300
-error0 = [np.sqrt(x)/time for x in counts0]
-error1 = [np.sqrt(x)/time for x in counts1]
-error2 = [np.sqrt(x)/time for x in counts2]
-error3 = [np.sqrt(x)/time for x in counts3]
-counts0[:] = [x/300 for x in counts0]
-counts1[:] = [x/300 for x in counts1]
-counts2[:] = [x/300 for x in counts2]
-counts3[:] = [x/300 for x in counts3]
-plt.errorbar(thresh, counts0, yerr = error0, fmt = 'k', label = 'CH0')
-plt.errorbar(thresh, counts1, yerr = error1, fmt = 'r', label = 'CH1')
-plt.errorbar(thresh, counts2, yerr = error2, fmt = 'g', label = 'CH2')
-plt.errorbar(thresh, counts3, yerr = error3, fmt = 'b', label = 'CH3')
-plt.ylabel("Events/Second")
-plt.xlabel('Threshold (mV)')
-plt.legend()
-plt.show()
+print("Channel 0 efficiency: ", count[0]/n0_coinc)
+print("Channel 1 efficiency: ", count[1]/n1_coinc)
+print("Channel 2 efficiency: ", count[2]/n2_coinc)
+print("Channel 3 efficiency: ", count[3]/n3_coinc)
+
